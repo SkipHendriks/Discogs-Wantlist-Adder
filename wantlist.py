94eb0f02caadb510db85ca0fd8d4e9bc82bfbf53
Skip Hendriks <s.hendriks@omnihis.nl>
2016-02-14 16:27:04 +0100

    Added user token and delimiter

--- wantlist.py
+++ wantlist.py
@@ -8,28 +8,23 @@ import sys
 
 DELIMITER = " || "
 
-d = discogs_client.Client(
-    'ExampleApplication/0.1',
-    user_token="zBgDuMUoWCUtzYVtmKiTykvGMTXwKfdDlSisujtt"
-)
 
-
-def readFile(filename):
+def readFile(filename, delimiter):
     with open(filename) as f:
         for line in f:
-            album = findAlbum(line)
+            album = findAlbum(line, delimiter)
             addAlbum(album)
 
 
-def readSTDIN(stdin):
+def readSTDIN(stdin, delimiter):
     for line in stdin:
-        album = findAlbum(line)
+        album = findAlbum(line, delimiter)
         addAlbum(album)
 
 
-def findAlbum(line):
-    artist = line[0:line.find(DELIMITER)]
-    album = line[line.find(DELIMITER)+len(DELIMITER):]
+def findAlbum(line, delimiter):
+    artist = line[0:line.find(delimiter)]
+    album = line[line.find(delimiter)+len(delimiter):]
 
     return d.search(
         artist=artist,
@@ -47,19 +42,24 @@ def addAlbum(album):
 
 def main():
 
+    global d
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "positional_file",
-        type=str,
-        nargs='?'
-    )
-
-    parser.add_argument(
-        'pipe_input_file',
+        'STDIN',
         nargs='?',
         type=argparse.FileType('r'),
         default=sys.stdin
+    )
+
+    parser.add_argument(
+        "-u",
+        "--user-token",
+        default=None,
+        type=str,
+        dest="USER_TOKEN",
+        help="User token to connect with the Discogs API. You can obtain a token from https://www.discogs.com/settings/developers"
     )
 
     parser.add_argument(
@@ -67,8 +67,17 @@ def main():
         "--file",
         default=None,
         type=str,
-        dest="optional_file",
-        help="File with album-titles and artists delimited by '"+DELIMITER+"' (not including quotes)"
+        dest="FILE",
+        help="File with album-titles and artists delimited by '"+DELIMITER+"' (not including quotes) or a chosen delimiter"
+    )
+
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        default=None,
+        type=str,
+        dest="DELIMITER",
+        help="Delimiter used in the specfied file. Defaults to '"+DELIMITER+"' (not including quotes) when not specified"
     )
 
     parser.add_argument(
@@ -78,19 +87,36 @@ def main():
         version='%(prog)s 0.1'
     )
 
+    parser.add_argument(
+        "-r",
+        "--revert",
+        action='store_true',
+        dest="REVERT",
+        help="Removes the specified files from your wantlist (not yet implemented)"
+    )
+
     options = parser.parse_args()
 
-    input_count = sum([bool(options.positional_file), bool(options.optional_file), not sys.stdin.isatty()])
+    input_count = sum([bool(options.FILE), not sys.stdin.isatty()])
 
     if input_count is 1:
-        if(options.positional_file or options.optional_file):
-            readFile(options.positional_file or options.optional_file)
+        if options.USER_TOKEN:
+            d = discogs_client.Client(
+                'ExampleApplication/0.1',
+                user_token=options.USER_TOKEN
+            )
         else:
-            readSTDIN(options.pipe_input_file)
+            raise RuntimeError("Please specify a user token")
+        delimiter = options.DELIMITER or " || "
+        if(options.FILE):
+            readFile(options.FILE, delimiter)
+        else:
+            readSTDIN(options.STDIN, delimiter)
     elif input_count is 0:
         raise RuntimeError("Please specify a file location")
     else:
-        raise RuntimeError("Please specify only one file")
+        raise RuntimeError("Please specify only file or stdin")
+
 
 if __name__ == "__main__":
     main()
