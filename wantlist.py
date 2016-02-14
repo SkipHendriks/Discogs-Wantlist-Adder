64dea9af29ec74757c666a27a8aa9ca9e44f2849
Skip Hendriks <s.hendriks@omnihis.nl>
2016-02-14 19:43:15 +0100

    Added revert functionality

--- wantlist.py
+++ wantlist.py
@@ -5,18 +5,33 @@ import discogs_client
 import argparse
 import sys
 
+# Todo:
+# 1. Add prompts for file and token
+# 2. Implement OAuth
+# 3. Add format option
+# 4. Refactoring:
+#    1. reading in main() ( will become handleUserInput())
+#    2. findAlbum() called from addAlbum()
+#    3. Add handleUserInput()
 
-def readFile(filename, delimiter):
+
+def readFile(filename, delimiter, revert):
     with open(filename) as f:
         for line in f:
             album = findAlbum(line, delimiter)
-            addAlbum(album)
+            if revert is True:
+                removeAlbum(album)
+            else:
+                addAlbum(album)
 
 
-def readSTDIN(stdin, delimiter):
+def readSTDIN(stdin, delimiter, *revert):
     for line in stdin:
         album = findAlbum(line, delimiter)
-        addAlbum(album)
+        if revert is True:
+            removeAlbum(album)
+        else:
+            addAlbum(album)
 
 
 def findAlbum(line, delimiter):
@@ -35,6 +50,12 @@ def addAlbum(album):
     pprint.pprint(vars(album))
     me = d.identity()
     me.wantlist.add(album)
+
+
+def removeAlbum(album):
+    pprint.pprint(vars(album))
+    me = d.identity()
+    me.wantlist.remove(album)
 
 
 def main():
@@ -89,28 +110,32 @@ def main():
         "--revert",
         action='store_true',
         dest="REVERT",
-        help="Removes the specified files from your wantlist (not yet implemented)"
+        help="Removes the specified files from your wantlist"
     )
 
     options = parser.parse_args()
+
+    print(options.REVERT)
 
     input_count = sum([bool(options.FILE), not sys.stdin.isatty()])
 
     if input_count is 1:
         if options.USER_TOKEN:
+
             global d
             d = discogs_client.Client(
-                'WantlistAdder/0.1',
+                'Discogs-Wantlist-Adder/0.1',
                 user_token=options.USER_TOKEN
             )
+
         else:
             raise RuntimeError("Please specify a user token")
         if options.DELIMITER:
             delimiter = options.DELIMITER
         if(options.FILE):
-            readFile(options.FILE, delimiter)
+            readFile(options.FILE, delimiter, options.REVERT)
         else:
-            readSTDIN(options.STDIN, delimiter)
+            readSTDIN(options.STDIN, delimiter, options.REVERT)
     elif input_count is 0:
         raise RuntimeError("Please specify a file location")
     else:
